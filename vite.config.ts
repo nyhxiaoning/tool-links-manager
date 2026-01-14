8e24586bd6444bf6eec62ef588a76b59532abb5b
woung <younglina0409@outlook.com>
2026-01-14 21:55:59 +0800

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  base: '/tool-links',
   plugins: [vue()],
   resolve: {
     alias: {
